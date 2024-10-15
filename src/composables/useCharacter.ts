d60b49a3389f87b1fa2e4b9d272ecbc92aef8cd6
Stian <stian.oek@gmail.com>
2024-10-15 23:48:48 +0200

    installed json-server for a mock db and persistant storage

--- src/composables/useCharacter.ts
+++ src/composables/useCharacter.ts
@@ -1,20 +1,17 @@
 // src/composables/useCharacters.ts
 import { ref } from "vue";
 import { Characters, Character } from "../types";
+import axios from "axios";
 
 const characterList = ref<Characters>([]);
 
 // Jeg bruker isFetched for å sjekke om vi allerede har hentet karakterene, dette er for å unngå refetch når jeg navigerer med router frem og tilbake.
-const isFetched = ref<boolean>(false);
-
 const fetchCharacters = async () => {
-  if (isFetched.value) return; // sjekk om vi allerede har hentet karakterene, hvis vi har det, ikke gjør noen ting
   try {
-    const response = await fetch("/src/characters.json");
+    const response = await fetch("http://localhost:3001/characters");
     if (response.ok) {
       const data = await response.json();
       characterList.value = data;
-      isFetched.value = true;
     } else {
       console.error("Failed to fetch characters:", response.statusText);
     }
@@ -23,8 +20,13 @@ const fetchCharacters = async () => {
   }
 };
 
-const addCharacter = (newCharacter: Character) => {
-  characterList.value.push(newCharacter);
+const addCharacter = async (newCharacter: Character) => {
+  const add = await axios.post("http://localhost:3001/characters", newCharacter);
+  if (add.status === 201) {
+    characterList.value.push(newCharacter);
+  } else {
+    console.error("Failed to add character:", add.statusText);
+  }
 };
 
 export function useCharacters() {
